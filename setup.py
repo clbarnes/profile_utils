39916c1e87da6deca325e3c7422e6d182dfd72a6
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2014-12-04 15:58:02 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='profile_utils',
+    version='',
+    packages=[''],
+    url='https://github.com/clbarnes/profile_utils',
+    license='MIT',
+    author='clbarnes',
+    author_email='cbarnes@mrc-lmb.cam.ac.uk',
+    description='Some utilities for help in profiling and timing code, because timeit is wank.'
+)
